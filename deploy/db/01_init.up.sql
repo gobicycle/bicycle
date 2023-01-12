00c36ced13e51197e701f393296f1bc06ac52829
gobicycle <captonom@gmail.com>
2023-01-12 03:37:56 +0300

    db deploy fix

--- deploy/db/01_init.up.sql
+++ deploy/db/01_init.up.sql
@@ -11,13 +11,13 @@ CREATE TABLE IF NOT EXISTS payments.ton_wallets
     address          bytea not null
 );
 
-CREATE INDEX ton_wallets_address_index
+CREATE INDEX IF NOT EXISTS ton_wallets_address_index
     ON payments.ton_wallets (address);
 
-CREATE INDEX ton_wallets_type_index
+CREATE INDEX IF NOT EXISTS ton_wallets_type_index
     ON payments.ton_wallets (type);
 
-CREATE INDEX ton_wallets_user_id_index
+CREATE INDEX IF NOT EXISTS ton_wallets_user_id_index
     ON payments.ton_wallets (user_id);
 
 CREATE TABLE IF NOT EXISTS payments.jetton_wallets
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS payments.jetton_wallets
     address          bytea not null unique
 );
 
-CREATE INDEX jetton_wallets_subwallet_id_index
+CREATE INDEX IF NOT EXISTS jetton_wallets_subwallet_id_index
     ON payments.jetton_wallets (subwallet_id);
 
 CREATE TABLE IF NOT EXISTS payments.internal_incomes
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS payments.internal_incomes
     unique (memo, lt)
 );
 
-CREATE INDEX internal_incomes_deposit_address_index
+CREATE INDEX IF NOT EXISTS internal_incomes_deposit_address_index
     ON payments.internal_incomes (deposit_address);
 
 CREATE TABLE IF NOT EXISTS payments.external_withdrawals
@@ -60,16 +60,16 @@ CREATE TABLE IF NOT EXISTS payments.external_withdrawals
     unique (msg_uuid, address)
 );
 
-CREATE INDEX external_withdrawals_expired_at_index
+CREATE INDEX IF NOT EXISTS external_withdrawals_expired_at_index
     ON payments.external_withdrawals (expired_at);
 
-CREATE INDEX external_withdrawals_msg_uuid_index
+CREATE INDEX IF NOT EXISTS external_withdrawals_msg_uuid_index
     ON payments.external_withdrawals (msg_uuid);
 
-CREATE INDEX external_withdrawals_address_index
+CREATE INDEX IF NOT EXISTS external_withdrawals_address_index
     ON payments.external_withdrawals (address);
 
-CREATE INDEX external_withdrawals_query_id_index
+CREATE INDEX IF NOT EXISTS external_withdrawals_query_id_index
     ON payments.external_withdrawals (query_id);
 
 CREATE TABLE IF NOT EXISTS payments.withdrawal_requests
@@ -90,13 +90,13 @@ CREATE TABLE IF NOT EXISTS payments.withdrawal_requests
     unique (user_id, user_query_id, is_internal)
 );
 
-CREATE INDEX withdrawal_requests_user_id_index
+CREATE INDEX IF NOT EXISTS withdrawal_requests_user_id_index
     ON payments.withdrawal_requests (user_id);
 
-CREATE INDEX withdrawal_requests_user_query_id_index
+CREATE INDEX IF NOT EXISTS withdrawal_requests_user_query_id_index
     ON payments.withdrawal_requests (user_query_id);
 
-CREATE INDEX withdrawal_requests_dest_address_index
+CREATE INDEX IF NOT EXISTS withdrawal_requests_dest_address_index
     ON payments.withdrawal_requests (dest_address);
 
 CREATE TABLE IF NOT EXISTS payments.external_incomes
@@ -109,7 +109,7 @@ CREATE TABLE IF NOT EXISTS payments.external_incomes
     comment          text not null
 );
 
-CREATE INDEX external_incomes_deposit_address_index
+CREATE INDEX IF NOT EXISTS external_incomes_deposit_address_index
     ON payments.external_incomes (deposit_address);
 
 CREATE TABLE IF NOT EXISTS payments.block_data
@@ -123,7 +123,7 @@ CREATE TABLE IF NOT EXISTS payments.block_data
         unique (shard, seqno)
 );
 
-CREATE INDEX block_data_seqno_index
+CREATE INDEX IF NOT EXISTS block_data_seqno_index
     ON payments.block_data (seqno);
 
 CREATE TABLE IF NOT EXISTS payments.internal_withdrawals
@@ -141,13 +141,13 @@ CREATE TABLE IF NOT EXISTS payments.internal_withdrawals
     unique (from_address, since_lt, memo)
 );
 
-CREATE INDEX internal_withdrawals_from_address_index
+CREATE INDEX IF NOT EXISTS internal_withdrawals_from_address_index
     ON payments.internal_withdrawals (from_address);
 
-CREATE INDEX internal_withdrawals_since_lt_index
+CREATE INDEX IF NOT EXISTS internal_withdrawals_since_lt_index
     ON payments.internal_withdrawals (since_lt);
 
-CREATE INDEX internal_withdrawals_expired_at_index
+CREATE INDEX IF NOT EXISTS internal_withdrawals_expired_at_index
     ON payments.internal_withdrawals (expired_at);
 
 CREATE TABLE IF NOT EXISTS payments.service_withdrawal_requests
