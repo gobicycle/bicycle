48dbbdf976a22107e5a91343ccacdc98af51669e
gobicycle <captonom@gmail.com>
2024-06-17 01:53:30 +0300

    migration fix

--- deploy/manual_migrations/0.4.x-0.5.0.sql
+++ deploy/manual_migrations/0.4.x-0.5.0.sql
@@ -4,7 +4,7 @@ ALTER TABLE payments.external_withdrawals
     ADD COLUMN IF NOT EXISTS tx_hash bytea;
 
 ALTER TABLE payments.withdrawal_requests
-    ADD COLUMN IF NOT EXISTS binary_comment text;
+    ADD COLUMN IF NOT EXISTS binary_comment text default '';
 
 ALTER TABLE payments.external_incomes
     ADD COLUMN IF NOT EXISTS tx_hash bytea;
