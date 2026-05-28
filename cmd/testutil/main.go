91a2ad26a367d5fc624995f38ddbbb8f115159ad
GoBicycle <120649456+gobicycle@users.noreply.github.com>
2026-05-28 21:42:44 +0300

    Update main.go

--- cmd/testutil/main.go
+++ cmd/testutil/main.go
@@ -53,7 +53,7 @@ func main() {
 		log.Fatalf("invalid HOT_WALLET_B env var")
 	}
 
-	bcClient, err := blockchain.NewConnection(config.Config.LiteServer, config.Config.LiteServerKey, config.Config.LiteServerRateLimit)
+	bcClient, err := blockchain.NewConnection(config.Config.LiteServer, config.Config.LiteServerKey, config.Config.LiteServerRateLimit, nil)
 	if err != nil {
 		log.Fatalf("blockchain connection error: %v", err)
 	}
